497bb57fa89a1e2bc15d9c468f3d4453178b7a20
Jaron <jaron_zijlstra@outlook.com>
2023-08-14 18:28:52 +0200

    use single quotes for strings with double quotes

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ async def on_ready():
 @bot.slash_command(name = "salute", description = "Gives a salute", guild_ids=[main_guild_id])
 async def salute(ctx):
     salutes = [
-        "\"Just who is the Enclave? Why, that's simple. The Enclave is you, America. The Enclave is your sister, your aunt, your friend, your neighbor.\" - Eden",
+        '"Just who is the Enclave? Why, that\'s simple. The Enclave is you, America. The Enclave is your sister, your aunt, your friend, your neighbor." - Eden',
         "One Enclave. One America. Now... and forever.",
-        "\"We will rebuild the American family, as it was, as it was meant to be!\" - Eden",
-        "\"The Enclave will restore peace, order, and prosperity to this great nation. And those who oppose us will be… removed. Forever.\" - Eden",
+        '"We will rebuild the American family, as it was, as it was meant to be!" - Eden',
+        '"The Enclave will restore peace, order, and prosperity to this great nation. And those who oppose us will be… removed. Forever." - Eden',
         "The values of our past... shall be the foundation of our future.",
-        "\"These power-armored boy scouts are nothing more than common criminals with access to some antiquated technology.\" - Eden",
+        '"These power-armored boy scouts are nothing more than common criminals with access to some antiquated technology." - Eden',
         "The price of freedom is eternal vigilance.",
-        "\"The government should not be guided by Temporary Excitement, but by Sober Second Thought.\" - Eden",
-        "\"May our country be always successful, but whether successful or otherwise, always right.\" - Eden"
+        '"The government should not be guided by Temporary Excitement, but by Sober Second Thought." - Eden',
+        '"May our country be always successful, but whether successful or otherwise, always right." - Eden'
     ] 
     await ctx.respond(random.choice(salutes))
 
