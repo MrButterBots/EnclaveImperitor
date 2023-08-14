e4fd029426346abc48c67789e5e3d7ee77190a1c
Jaron <jaron_zijlstra@outlook.com>
2023-08-14 16:59:45 +0200

    initial bot

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+import os
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+bot = discord.Bot()
+main_guild_id = "1131372079061667993"
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name = "salute", description = "Gives a salute", guild_ids=[main_guild_id])
+async def salute(ctx):
+    salutes = [
+        "\"Just who is the Enclave? Why, that's simple. The Enclave is you, America. The Enclave is your sister, your aunt, your friend, your neighbor.\" - Eden",
+        "One Enclave. One America. Now... and forever.",
+        "\"We will rebuild the American family, as it was, as it was meant to be!\" - Eden",
+        "\"The Enclave will restore peace, order, and prosperity to this great nation. And those who oppose us will be… removed. Forever.\" - Eden",
+        "The values of our past... shall be the foundation of our future.",
+        "\"These power-armored boy scouts are nothing more than common criminals with access to some antiquated technology.\" - Eden",
+        "The price of freedom is eternal vigilance.",
+        "\"The government should not be guided by Temporary Excitement, but by Sober Second Thought.\" - Eden",
+        "\"May our country be always successful, but whether successful or otherwise, always right.\" - Eden"
+    ] 
+    await ctx.respond(random.choice(salutes))
+
+@bot.slash_command(name = "suuid", description = "Generated a short user id", guild_ids=[main_guild_id])
+async def suuid(ctx):
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    suuid = [None] * 4
+    for i in range(4):
+        suuid[i] = ""
+        for j in range(4):
+            suuid[i] += random.choice(chars)
+    suuid = "-".join(str(x) for x in suuid)
+    await ctx.respond(suuid)
+
+bot.run(os.getenv("TOKEN"))
+
