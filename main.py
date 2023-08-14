19ed77ee4937e78c66a69382df490d2374c2facd
Jaron <jaron_zijlstra@outlook.com>
2023-08-14 23:36:33 +0200

    add meme command

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import discord
 import os
 import random
 import uuid
+import urllib.request
+import json
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 bot = discord.Bot()
@@ -42,6 +45,54 @@ async def roll(ctx):
         color=discord.Colour.blue()
     )
     await ctx.respond(embed=embed)
+
+@bot.slash_command(name = "meme", description = "Show a random meme", guild_ids=[main_guild_id])
+async def meme(ctx):
+    subreddits = [
+        "ProgrammerHumor",
+        "softwaregore",
+        "memes",
+        "AdviceAnimals",
+        "MemeEconomy",
+        "ComedyCemetery",
+        "dankmemes",
+        "PrequelMemes",
+        "terriblefacebookmemes",
+        "bonehurtingjuice"
+    ]
+    chosen = random.choice(subreddits)
+    url = f"https://reddit.com/r/{chosen}/.json"
+    response = urllib.request.urlopen(url).read()
+    responseJson = json.loads(response)
+    memes = responseJson["data"]["children"]
+    memesData = [None] * len(memes)
+    for i in range(len(memes)):
+        memesData[i] = memes[i]["data"]
+    filteredMemes = list(filter(allowed_meme, memesData))
+    meme = random.choice(filteredMemes)
+    permalink = meme["permalink"]
+    author = meme["author"]
+    subreddit = meme["subreddit_name_prefixed"]
+    ups = meme["ups"]
+    downs = meme["downs"]
+    created = meme["created"]
+
+    embed = discord.Embed(
+        title = meme["title"],
+        description = f"From {subreddit}",
+        color = discord.Colour.blue(),
+        url = f"https://reddit.com{permalink}",
+        timestamp = datetime.fromtimestamp(created)
+    )
+
+    embed.set_image(url = meme["url"])
+    embed.set_author(name = f"u/{author}")
+    embed.set_footer(text = f"⬆️ {ups} ⬇️ {downs}")
+
+    await ctx.respond(embed=embed)
+
+def allowed_meme(meme):
+    return not meme["over_18"] and not meme["is_video"]
 
 bot.run(os.getenv("TOKEN"))
 
